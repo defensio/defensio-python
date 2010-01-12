f4c4e830b2a89a151aef71231e8335accd9c4a8f
Carl Mercier <carl@carlmercier.com>
2010-01-12 10:16:54 -0500

    added url

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ from setuptools import setup
 setup(
     name = 'defensio',
     version = '0.9',
+    url = "http://github.com/defensio/defensio-python",
     description = 'Official Python library for Defensio 2.0',
     author = 'Camilo Lopez',
     author_email = 'clopez@websense.com',
     packages = ['defensio'],
     long_description='Official Python library for Defensio 2.0',
-      classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Internet",
-        ],
-      keywords='defensio spam antispam api',
-      license='MIT'
+    classifiers=[
+      "License :: OSI Approved :: MIT License",
+      "Programming Language :: Python",
+      "Development Status :: 4 - Beta",
+      "Intended Audience :: Developers",
+      "Topic :: Internet",
+      ],
+    keywords='defensio spam antispam api',
+    license='MIT'
     )

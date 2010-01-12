31c5ea83616e257f3a6b81b0ac520bbb0925b1aa
Carl Mercier <carl@carlmercier.com>
2010-01-12 10:15:26 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup
 setup(
     name = 'defensio',
     version = '0.9',
-    description = 'Small but full featured library for Defensio 2.0',
+    description = 'Official Python library for Defensio 2.0',
     author = 'Camilo Lopez',
     author_email = 'clopez@websense.com',
     packages = ['defensio'],
-    long_description="""
-            Easily access Defensio anti-spam web service version 2.0
-      """,
+    long_description='Official Python library for Defensio 2.0',
       classifiers=[
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Internet",
         ],
-      keywords='defensio spam api',
-      license='GPL'
+      keywords='defensio spam antispam api',
+      license='MIT'
     )
